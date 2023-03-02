fac16a67549c1a78e12729c2b8b69fef65d327f4
Javier <javi.ssweb@gmail.com>
2023-03-02 20:35:30 +0100

    Creando Entity beer

---
+++ src/main/java/com/example/entities/Beer.java
@@ -0,0 +1,41 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "beer")
+
+public class Beer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String name;
+    private Double price;
+    private String color;
+    private Double alcohol;
+    private Boolean available;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Beer beer = (Beer) o;
+        return id != null && Objects.equals(id, beer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
